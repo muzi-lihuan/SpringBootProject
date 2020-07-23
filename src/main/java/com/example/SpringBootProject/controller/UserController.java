f217c03e74061df474b780590609c1631d54aa29
muzi-lihuan <62868968+muzi-lihuan@users.noreply.github.com>
2020-07-23 18:17:18 +0900

    Update UserController.java

--- src/main/java/com/example/SpringBootProject/controller/UserController.java
+++ src/main/java/com/example/SpringBootProject/controller/UserController.java
@@ -1,52 +1,21 @@
 package com.example.SpringBootProject.controller;
 
 import com.example.SpringBootProject.model.User;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.*;
-import springfox.documentation.annotations.ApiIgnore;
 
-/**GetMapping
- * @Api 注解，可以给控制器增加描述和标签信息。
- * @ApiOperation 注解来展开对接口的描述，当然这个注解还可以指定很多内容
- * @ApiIgnore: Swagger 文档不会显示拥有该注解的接口。
- * @ApiImplicitParams: 用于描述接口的非对象参数集。
- * @ApiImplicitParam: 用于描述接口的非对象参数，一般与 @ApiImplicitParams 组合使用。
+/**
  * @author ganchaoyang
  * @date 2019/3/1013:55
  */
 @RestController
 @RequestMapping("/user")
-@Api(tags = "用户相关接口", description = "提供用户相关的Rest API")
 public class UserController {
 
-    @ApiOperation("新增用户接口")
-    @PostMapping("/add")
-    public boolean addUser(@RequestBody User user) {
-        return false;
-    }
-
-    @ApiOperation("通过id查找用户接口")
-    @GetMapping("/find/{id}")
-    public User findById(@PathVariable("id") int id) {
-        return new User();
-    }
-
-    @ApiOperation("更新用户信息接口")
-    @PutMapping("/update")
-    public boolean update(@RequestBody User user) {
-        return true;
-    }
-
-    /**
-     * 如果想在文档中屏蔽掉删除用户的接口（user/delete），那么只需要在删除用户的方法上加上 @ApiIgnore 即可
-     * @param id
-     * @return
-     */
-    @ApiOperation("删除用户接口")
-    @DeleteMapping("/delete/{id}")
-    @ApiIgnore
-    public boolean delete(@PathVariable("id") int id) {
-        return true;
+    @GetMapping("/find")
+    public User findById() {
+        User user = new User();
+        user.setName("lisi");
+        user.setAddress("科技二路");
+        return user;
     }
 }
